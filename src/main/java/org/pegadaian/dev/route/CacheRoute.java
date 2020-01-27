0178525a9d2892bb5b4ae15555295bf978965c9c
TobiasMarsena <tmarsena@gmail.com>
2020-01-27 14:33:16 +0700

    Java DSL

---
+++ src/main/java/org/pegadaian/dev/route/CacheRoute.java
@@ -0,0 +1,36 @@
+package org.pegadaian.dev.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CacheRoute extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		
+		from("direct:putCache")
+			.id("put-cache")
+			.bean("greetService", "greetUser")
+			.to("direct:putCacheGreeting")
+		;
+		
+		from("direct:putCacheGreeting")
+			.id("greeting-to-cache")
+			.marshal().json(JsonLibrary.Jackson)
+			.convertBodyTo(String.class)
+			.setHeader("CamelInfinispanOperation", constant("CamelInfinispanOperationPut"))
+			.setHeader("CamelInfinispanValue", body())
+			.setHeader("CamelInfinispanKey", constant("Greeting"))
+			.setHeader("CamelInfinispanLifespanTime", constant("20s"))
+			.log("Sending body >>>>> ${body} >>>>> to cache")
+			.to("infinispan:{{jdg.url}}")
+			.log("Success save to cache jboss data grid: ${body}")
+			.unmarshal().json(JsonLibrary.Jackson)
+		;
+		
+	}
+
+	
+}
