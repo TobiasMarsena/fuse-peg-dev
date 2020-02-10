39f0aa4355bf4d35743afc925d68f0634b8887db
TobiasMarsena <tmarsena@gmail.com>
2020-02-10 10:13:30 +0700

    Rest DSL

--- src/main/java/org/pegadaian/dev/route/CacheRoute.java
+++ src/main/java/org/pegadaian/dev/route/CacheRoute.java
@@ -2,7 +2,11 @@ package org.pegadaian.dev.route;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.model.rest.RestOperationResponseMsgDefinition;
+import org.apache.camel.model.rest.VerbDefinition;
 import org.pegadaian.dev.CacheService;
+import org.pegadaian.dev.Greeting;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,6 +19,21 @@ public class CacheRoute extends RouteBuilder {
 			.handled(true)
 			.maximumRedeliveries(2)
 		;
+		
+		rest().bindingMode(RestBindingMode.json).enableCORS(true).path("/hello").description("Greet Service")
+			.get().id("list").outType(Greeting.class).produces("application/json").path("/{name}")
+				.description("Greet user that use this service")
+				.responseMessage().code(200).message("All users successfully returned").endResponseMessage()
+				.to("bean:greetService?method=greetUser")
+			.get().id("cache").outType(Greeting.class).produces("application/json").path("/list/cache")
+				.description("Return all cache")
+				.responseMessage().code(200).message("All cache successfully returned").endResponseMessage()
+				.to("direct:getCacheGreeting")
+			.get().id("putCache").consumes("application/json").path("/cache/{name}")
+				.description("Put cache data")
+				.responseMessage().code(200).message("Data successfully cached").endResponseMessage()
+				.to("direct:putCache")
+			;
 		
 		from("direct:putCache")
 			.id("put-cache")
